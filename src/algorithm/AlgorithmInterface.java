f1322ead1c5e345427be9613b81be2fd66f4f9b5
ucabpa3 <ucabpa3@ucl.ac.uk>
2013-07-03 20:30:21 +0100

    added algorithm interface

---
+++ src/algorithm/AlgorithmInterface.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author costas
+ */
+public interface AlgorithmInterface {
+    
+    public void execute(String fileName);
+    
+}
