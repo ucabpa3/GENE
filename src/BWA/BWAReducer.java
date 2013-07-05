5d67778d0a0618178a18323da04806a36f8b24f3
chenhao <milochen89@yahoo.com>
2013-07-05 15:11:29 +0100

    changes to reducer

--- src/BWA/BWAReducer.java
+++ src/BWA/BWAReducer.java
@@ -27,10 +27,10 @@ public class BWAReducer
         public void reduce(Text key, Iterable<IntWritable> values,
                 Context context) throws IOException, InterruptedException {
         	
-        	/* Configuration conf = new Configuration();  
-        	    String inputDir = new String("user/costas");//设定输入目录 
+        	 Configuration conf = new Configuration();  
+        	    String inputDir = "hdfs://localhost:9000/user/costas/";//设定输入目录 
         	    FileSystem hdfs =FileSystem.get(URI.create(inputDir),conf); //获得HDFS文件系统的对象
-        	    Path hdfsFile = new Path(args[1]);//设定输出目录  
+        	    Path hdfsFile = "hdfs://localhost:9000/user/costas/output.sai";//设定输出目录  
         	   try{
         		   
         	      FileStatus[] inputFiles = hdfs.listStatus(new Path(inputDir));//FileStatus的listStatus()方法获得一个目录中的文件列表 
@@ -38,17 +38,18 @@ public class BWAReducer
         	      for(int i = 0; i < inputFiles.length; i ++){  
         	            System.out.println(inputFiles[i].getPath().getName());
         	            String extensionName = getExtensionName(inputFiles[i].getPath().getName()).toLowerCase();
-        	            if (extensionName == "sai"){
+        	            System.out.println(extensionName);
+        	            if (extensionName.equals("sai")){
         	            FSDataInputStream in = hdfs.open(inputFiles[i].getPath());//打开本地输入流  
         	            byte[] buffer = new byte[256];  
         	            int bytesRead = 0;  
         	            while((bytesRead = in.read(buffer))>0){  
         	            out.write(buffer,0,bytesRead);//通过一个循环来写入  
-        	          }  
-        	            in.close();  
-        	         }  
-        	         out.close();
+        	            }
+        	            in.close();
+        	        }     
         	      }
+        	      	out.close();
         	     }catch (IOException e) {  
         	           e.printStackTrace();  
         	     }  	
@@ -64,6 +65,5 @@ public class BWAReducer
         	}
         	}
         	return filename;
-        	} */
+        	} 
     }
-}
