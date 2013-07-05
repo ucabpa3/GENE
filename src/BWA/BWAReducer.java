020f6cc89b55cfb0dbc9d1e0b203c886a4cabb50
chenhao <milochen89@yahoo.com>
2013-07-05 14:37:17 +0100

    changed the reducer

--- src/BWA/BWAReducer.java
+++ src/BWA/BWAReducer.java
@@ -56,13 +56,13 @@ public class BWAReducer
              //   context.write(key, val);
             //}
         }
+        public static String getExtensionName(String filename) {
+        	if ((filename != null) && (filename.length() > 0)) {
+        	int dot = filename.lastIndexOf('.');
+        	if ((dot >-1) && (dot < (filename.length() - 1))) {
+        	return filename.substring(dot + 1);
+        	}
+        	}
+        	return filename;
+        	} 
     }
-public static String getExtensionName(String filename) {
-	if ((filename != null) && (filename.length() > 0)) {
-	int dot = filename.lastIndexOf('.');
-	if ((dot >-1) && (dot < (filename.length() - 1))) {
-	return filename.substring(dot + 1);
-	}
-	}
-	return filename;
-	} 
